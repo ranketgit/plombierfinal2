8e97399601175035368d3c76769798b50f9d824e
ranketgit <jalal@ranket.co>
2024-11-29 11:57:24 +0100

    removing redirections from next config

--- next.config.js
+++ next.config.js
@@ -1,33 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      // Redirect /[province]/[commune] to /plombier-[commune]
-      {
-        source: '/:province/:commune',
-        destination: '/plombier-:commune',
-        permanent: true,
-      },
-      // Redirect /[province] to /plombiers-province-[province]
-      {
-        source: '/:province',
-        destination: '/plombiers-province-:province',
-        permanent: true,
-      },
-      // Redirect /[ville] to /plombiers-province-[province]
-      {
-        source: '/:ville',
-        destination: '/plombiers-province-:ville',
-        permanent: true,
-      },
-      // Redirect /[ville]/[commune] to /plombier-[commune]
-      {
-        source: '/:ville/:commune',
-        destination: '/plombier-:commune',
-        permanent: true,
-      },
-    ]
-  },
+
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig

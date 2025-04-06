6bb130d96f23f5d0c479b934b101074f887f2442
BenMiloud <35612329+BenMiloud@users.noreply.github.com>
2025-04-06 20:57:52 +0200

    Updated locations and getStaticPaths function.

--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -1,16 +1,19 @@
-export function generateSchema(location: string) {
+export function generateSchema(
+  title: string,
+  description: string,
+  ratingCount: number = 37,
+) {
   return {
     "@context": "http://schema.org",
-    "@type": "Product",
-    name: `Plombier ${location} en Urgence 24/7`,
+    name: `${title}`,
     brand: "Plombier en Urgence",
     image: "https://express-debouchage.com/plombier-en-urgence.jpg",
-    description: `Plombier ${location}. Intervention rapide 24/7 par un plombier agréé. Dépannage d'urgence, installation, réparation. ☎ 0472/80.80.80`,
+    description: `${description}`,
     sku: "0472800009",
     aggregateRating: {
       "@type": "AggregateRating",
       ratingValue: "9.9",
-      ratingCount: "11",
+      ratingCount: `${ratingCount}`,
       bestRating: "10",
       worstRating: "1",
     },
